94e2392e1ab60d958736b65124ad0fc76361d2ac
Felipe Nogueira <contato.fnog@gmail.com>
2023-02-10 16:42:34 -0300

    Remove multiple object initializations

--- cpf.test.ts
+++ cpf.test.ts
@@ -1,31 +1,26 @@
 import CPF from "./cpf";
+const cpf = new CPF();
 
 describe('CPF validation', function () {
   test("CPF w/o dots is valid", () => {
-    const cpf = new CPF();
     expect(cpf.isValidCPF("13618365047")).toBe(true);
   });
   test("CPF w/dots is valid", () => {
-    const cpf = new CPF();
     expect(cpf.isValidCPF("136.183.650-47")).toBe(true);
   });
   test("invalid CPF returns false", () => {
-    const cpf = new CPF();
     expect(cpf.isValidCPF("1361865047")).toBe(false);
   });
   test("Invalid CPF returns false", () => {
-    const cpf = new CPF();
     expect(cpf.isValidCPF("136183650447")).toBe(false);
   });
 });
 
 describe('Formatted output', function () {
   test("CPF w/o dots returns formatted CPF", () => {
-    const cpf = new CPF();
     expect(cpf.formatCPF("13618365047")).toEqual("136.183.650-47");
   });
   test("Formatted CPF returns CPF w/o dots", () => {
-    const cpf = new CPF();
     expect(cpf.formatCPF("136.183.650-47")).toEqual("13618365047");
   });
 });
