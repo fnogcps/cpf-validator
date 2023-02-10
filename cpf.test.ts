ebb65bc84d4a9c6305e812db1ff2d6dd8f896735
Felipe Nogueira <contato.fnog@gmail.com>
2023-02-10 16:38:13 -0300

    Add tests for format validation

--- cpf.test.ts
+++ cpf.test.ts
@@ -1,16 +1,31 @@
 import CPF from "./cpf";
 
 describe('CPF validation', function () {
-  test("Is CPF without dots valid", () => {
+  test("CPF w/o dots is valid", () => {
     const cpf = new CPF();
     expect(cpf.isValidCPF("13618365047")).toBe(true);
   });
-  test("Is formated CPF valid", () => {
+  test("CPF w/dots is valid", () => {
     const cpf = new CPF();
     expect(cpf.isValidCPF("136.183.650-47")).toBe(true);
   });
-  test("Invalid CPF returns false", () => {
+  test("invalid CPF returns false", () => {
     const cpf = new CPF();
     expect(cpf.isValidCPF("1361865047")).toBe(false);
+  });
+  test("Invalid CPF returns false", () => {
+    const cpf = new CPF();
+    expect(cpf.isValidCPF("136183650447")).toBe(false);
+  });
+});
+
+describe('Formatted output', function () {
+  test("CPF w/o dots returns formatted CPF", () => {
+    const cpf = new CPF();
+    expect(cpf.formatCPF("13618365047")).toEqual("136.183.650-47");
+  });
+  test("Formatted CPF returns CPF w/o dots", () => {
+    const cpf = new CPF();
+    expect(cpf.formatCPF("136.183.650-47")).toEqual("13618365047");
   });
 });
