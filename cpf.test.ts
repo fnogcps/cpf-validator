d53855179e2cddd1b23dc1e6c374b061458e2b5f
Felipe Nogueira <contato.fnog@gmail.com>
2023-02-10 11:46:59 -0300

    Add more validate tests

--- cpf.test.ts
+++ cpf.test.ts
@@ -1,6 +1,16 @@
 import CPF from "./cpf";
 
-test("Test if CPF is valid", () => {
-  const cpf = new CPF();
-  expect(cpf.isValidCPF("13618365047")).toBe(true);
+describe('CPF validation', function () {
+  test("Is CPF without dots valid", () => {
+    const cpf = new CPF();
+    expect(cpf.isValidCPF("13618365047")).toBe(true);
+  });
+  test("Is formated CPF valid", () => {
+    const cpf = new CPF();
+    expect(cpf.isValidCPF("136.183.650-47")).toBe(true);
+  });
+  test("Invalid CPF returns false", () => {
+    const cpf = new CPF();
+    expect(cpf.isValidCPF("1361865047")).toBe(false);
+  });
 });
